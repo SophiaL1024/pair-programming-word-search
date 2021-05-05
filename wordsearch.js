fc591bc704cc22b1c07fd93e772e1646ba19c3b6
SophiaL1024 <sophiafengliu@gmail.com>
2021-05-05 23:26:25 +0000

    pair programming word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,40 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function (matrix) {
+    let newMatrix = [];
+    let length = matrix[0].length;
+  
+    for (let outerArr = 0; outerArr < length; outerArr++) {
+      let newRow = [];
+      for (let innerArr = 0; innerArr < matrix.length; innerArr++) {
+        newRow.push(matrix[innerArr].shift());
+      }
+      newMatrix.push(newRow);
     }
-}
-
-module.exports = wordSearch
+    return newMatrix;
+  };
+  
+  const wordSearch = (letters, word) => { 
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  
+    const backwardsWord = word.split('').reverse().join('');
+  
+      for (l of horizontalJoin) {
+          if (l.includes(word)) return true;
+      }
+  
+      for (l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+  
+    for (l of horizontalJoin) {
+      if (l.includes(backwardsWord)) return true;
+  }
+  
+    for (l of verticalJoin) {
+      if (l.includes(backwardsWord)) return true;
+  }
+      return false;
+  }
+  
+  
+  module.exports = wordSearch
